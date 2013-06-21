8a3e7cc1738a43a98c07f0eb4071288c4962174a
Michael Cannon <michael@typo3vagabond.com>
2013-06-21 10:52:16 -0700

    Review WP SEO by Yoast changes
    * Protect env vars
    * Pull over helper methods
    * Add in other PHP and WP versions

--- ci/tests/test-sample.php
+++ ci/tests/test-sample.php
@@ -5,6 +5,15 @@ class SampleTest extends WP_UnitTestCase {
 		$this->assertTrue( true );
 	}
 
+	// Put convenience methods here
+	// Here are two I use for faking things for save_post hooks, et al
+	function set_post( $key, $value ) {
+		$_POST[$key] = $_REQUEST[$key] = addslashes( $value );
+	}
+
+	function unset_post( $key ) {
+		unset( $_POST[$key], $_REQUEST[$key] );
+	}
 
 }
 
