4a16b01bd0c16452670d636ca11094d6e54c2a39
Michael Cannon <michael@typo3vagabond.com>
2013-06-21 01:36:23 -0700

    Push to ci directory

--- ci/tests/bootstrap.php
+++ ci/tests/bootstrap.php
@@ -3,7 +3,7 @@
 require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../testimonials-widget.php';
+	require dirname( __FILE__ ) . '/../../testimonials-widget.php';
 }
 
 
