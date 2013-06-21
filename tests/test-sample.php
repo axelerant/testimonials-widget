03861ab0e6c01666932c64b4f5c185f5b081243c
Michael Cannon <michael@typo3vagabond.com>
2013-06-21 00:44:59 -0700

    Test tests

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -8,7 +8,7 @@ class SampleTest extends WP_UnitTestCase {
 	}
 
 
-	function testSample() {
+	function XtestSample() {
 		// replace this with some actual testing code
 		$this->assertTrue( true );
 	}
