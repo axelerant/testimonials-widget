e798edf6b017be14115baa5d40dfcac18311eb5b
Michael Cannon <michael@typo3vagabond.com>
2013-06-21 00:39:39 -0700

    Try straight WordPress ruleset

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,9 @@ require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../testimonials-widget.php';
 }
+
+
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
-
+?>
--- tests/test-sample.php
+++ tests/test-sample.php
@@ -2,9 +2,19 @@
 
 class SampleTest extends WP_UnitTestCase {
 
+	function test_sample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+
+
 	function testSample() {
 		// replace this with some actual testing code
 		$this->assertTrue( true );
 	}
+
+
 }
 
+
+?>
