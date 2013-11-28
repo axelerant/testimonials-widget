894ffa51665b341aa1813899205fbfbe5dc60146
Michael Cannon <mc@aihr.us>
2013-11-27 16:07:23 -0800

    Squashed 'lib/aihrus/' changes from 82e3e8e..638eb9b
    638eb9b Verbiage updates
    847d6ac Use aihrus framework
    34c70c0 Coding standard
    36f0b27 Verbiage update
    63ba7da Tweaks from working with WordPress SEO
    
    git-subtree-dir: lib/aihrus
    git-subtree-split: 638eb9b5de568392d4a8f8b0905cb3153d144f53

--- class-aihrus-settings.php
+++ class-aihrus-settings.php
@@ -675,10 +675,10 @@ abstract class Aihrus_Settings {
 
 
 	/**
-	 * Let values like "true, 'true', 1, and 'yes'" to be true. Else, false
+	 * Let values like true, 'true', 1, 'on', and 'yes' to be true. Else, false
 	 */
 	public static function is_true( $value = null, $return_boolean = true ) {
-		if ( true === $value || 'true' == strtolower( $value ) || 1 == $value || 'yes' == strtolower( $value ) ) {
+		if ( true === $value || 'true' == strtolower( $value ) || 1 == $value || 'on' == strtolower( $value ) || 'yes' == strtolower( $value ) ) {
 			if ( $return_boolean )
 				return true;
 			else
@@ -718,6 +718,8 @@ abstract class Aihrus_Settings {
 		foreach ( static::$styles as $style )
 			echo $style;
 	}
+
+
 }
 
 
