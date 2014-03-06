0c1dfe1b00d6a29d2346aa0018bdb09b6a920e3b
Michael Cannon <mc@aihr.us>
2014-03-06 09:34:52 +0700

    Require Aihrus 1.0.4RC2

--- testimonials-widget.php
+++ testimonials-widget.php
@@ -31,7 +31,7 @@ if ( ! defined( 'ABSPATH' ) )
 	exit;
 
 if ( ! defined( 'TW_AIHR_VERSION' ) )
-	define( 'TW_AIHR_VERSION', '1.0.3' );
+	define( 'TW_AIHR_VERSION', '1.0.4RC2' );
 
 if ( ! defined( 'TW_BASE' ) )
 	define( 'TW_BASE', plugin_basename( __FILE__ ) );
