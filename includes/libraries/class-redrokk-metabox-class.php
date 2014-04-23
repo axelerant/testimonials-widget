ac120cca9449c103089cc61e9a7e3c2bc971516d
Michael Cannon <mc@aihr.us>
2014-04-23 09:51:18 +0800

    Version 1.1.3RC1
    * RESOLVE michael-cannon/testimonials-widget#85 New testimonial rating field is blank

--- includes/libraries/class-redrokk-metabox-class.php
+++ includes/libraries/class-redrokk-metabox-class.php
@@ -909,7 +909,7 @@ jQuery('#edit_<?php echo $this->_category_name; ?>_<?php echo $meta_id; ?>').cli
 			}
 ?>
 			<?php switch ( $type ) { default: ?>
-			<?php if ( is_callable( $type ) && function_exists( $type ) && ! empty( $custom[ $id ] ) ) : ?>
+			<?php if ( is_callable( $type ) && function_exists( $type ) ) : ?>
 			<tr>
 				<th scope="row" style="width: 140px">
 					<label for="<?php echo $id; ?>"><?php echo $name; ?></label>
