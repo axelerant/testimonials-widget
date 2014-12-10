0db705b9f9ccca95495669dd267ade98e985b5cb
Michael Cannon <mc@aihr.us>
2014-12-09 21:10:40 -0500

    Description update

--- includes/class-testimonials-widget-settings.php
+++ includes/class-testimonials-widget-settings.php
@@ -200,7 +200,7 @@ class Testimonials_Widget_Settings extends Aihrus_Settings {
 		self::$settings['slide_width'] = array(
 			'section' => 'widget',
 			'title' => esc_html__( 'Slide Width?' ),
-			'desc' => esc_html__( 'Width of each testimonial. Increase to thumbnail width or greater for horizontal transitions or when defaults fail.' ),
+			'desc' => esc_html__( 'Width of each testimonial. Increase to thumbnail width or greater for improved looks. Requried for horizontal transitions.' ),
 			'validate' => 'intval',
 			'std' => 0,
 		);
