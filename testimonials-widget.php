01665d224279323026312794bc565fc252bd5357
Michael Cannon <mc@aihr.us>
2014-12-13 11:40:08 -0500

    Require Aihrus Framework 1.1.6

--- testimonials-widget.php
+++ testimonials-widget.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'TW_AIHR_VERSION' ) ) {
-	define( 'TW_AIHR_VERSION', '1.1.6RC2' );
+	define( 'TW_AIHR_VERSION', '1.1.6' );
 }
 
 if ( ! defined( 'TW_BASE' ) ) {
