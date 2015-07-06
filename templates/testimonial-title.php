ff14d01785c7353cebc74e8ad8c2f480f03393a7
Michael Cannon <mc@aihr.us>
2015-07-06 20:18:47 +0530

    Changed layout of the titile

--- templates/testimonial-title.php
+++ templates/testimonial-title.php
@@ -1,7 +1,19 @@
 <?php
 global $tw_template_args;
 
+$atts          = $tw_template_args['atts'];
+$use_quote_tag = $atts['use_quote_tag'];
+
 $testimonial = $tw_template_args['testimonial'];
 $title = $testimonial['testimonial_source'];
+
+if ( ! $use_quote_tag ) {
+	?>
+	<blockquote><span class="list-title"><?php echo $title; ?></span>
+	<?php
+} else {
+	?>
+	<q><span class="list-title"><?php echo $title; ?></span></q>
+	<?php
+}
 ?>
-<span class="list-title"><?php echo $title; ?></span>
