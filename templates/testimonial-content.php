a3463b0fab4c77c72d594086f2f51794777ca9b9
Michael Cannon <michael@axelerant.com>
2015-07-16 12:28:11 -0400

    Coding standard

--- templates/testimonial-content.php
+++ templates/testimonial-content.php
@@ -7,7 +7,7 @@ $widget_number = $tw_template_args['widget_number'];
 
 $char_limit    	= $atts['char_limit'];
 $content_more  	= apply_filters( 'tw_content_more', esc_html__( '…', 'testimonials-widget' ) );
-$content_more 	.= Testimonials_Widget::$tag_close_quote;
+$content_more  .= Testimonials_Widget::$tag_close_quote;
 $do_content    	= ! $atts['hide_content'] && ! empty( $testimonial['testimonial_content'] );
 $do_title		= ! $atts['hide_source_title'] && ! empty( $testimonial['testimonial_source'] );
 $use_quote_tag 	= $atts['use_quote_tag'];
@@ -26,8 +26,8 @@ if ( $do_content ) {
 	//Prepand Title to content if "Hide Title Above Content?" is active from Testimonials Widget settings.
 	if ( $do_title && ! is_single() ) {
 		$testimonial_title 		= $testimonial['testimonial_source'];
-		$testimonial_title_html	= '<span class="list-title">'.$testimonial_title.'</span><br/>';
-		$content 				= $testimonial_title_html.$content;
+		$testimonial_title_html	= '<span class="list-title">' . $testimonial_title . '</span><br/>';
+		$content 				= $testimonial_title_html . $content;
 	}
 
 	if ( ! $use_quote_tag ) {
