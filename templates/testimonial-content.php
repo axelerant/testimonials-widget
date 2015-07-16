b0a580aa7d5bb7c7dd7b683f98b6017e734acf96
Saurabh Dixit <saurabhsirdixit@gmail.com>
2015-07-16 17:05:54 +0530

    Modified template to prepand Testimonial title to list and slider.

--- templates/testimonial-content.php
+++ templates/testimonial-content.php
@@ -9,6 +9,7 @@ $char_limit    = $atts['char_limit'];
 $content_more  = apply_filters( 'tw_content_more', esc_html__( '…', 'testimonials-widget' ) );
 $content_more .= Testimonials_Widget::$tag_close_quote;
 $do_content    = ! $atts['hide_content'] && ! empty( $testimonial['testimonial_content'] );
+$do_title	   = ! $atts['hide_source_title'] && ! empty( $testimonial['testimonial_source'] );
 $use_quote_tag = $atts['use_quote_tag'];
 
 if ( $do_content ) {
@@ -22,28 +23,21 @@ if ( $do_content ) {
 	$content = apply_filters( 'tw_content', $content, $widget_number, $testimonial, $atts );
 	$content = make_clickable( $content );
 
-	//Add <blockquote> open tag if "Hide Title Above Content?" is active from Testimonials Widget settings.
-	if ( ! $atts['hide_source_title'] ) {
-		if ( ! $use_quote_tag ) {
-			?>
-			<?php echo $content; ?></blockquote>
-			<?php
-		} else {
-			?>
-			<?php echo $content; ?></q>
-			<?php
-		}
-	} else {
-		if ( ! $use_quote_tag ) {
-			?>
-			<blockquote><?php echo $content; ?></blockquote>
-			<?php
-		} else {
-			?>
-			<q><?php echo $content; ?></q>
-			<?php
-		}
+	//Prepand Title to content if "Hide Title Above Content?" is active from Testimonials Widget settings.
+	if ( $do_title && ! is_single() ) {
+		$testimonial_title 			= $testimonial['testimonial_source'];
+		$testimonial_title_html 	= '<span class="list-title">'.$testimonial_title.'</span><br/>';
+		$content					= $testimonial_title_html.$content;
 	}
 
+	if ( ! $use_quote_tag ) {
+		?>
+		<blockquote><?php echo $content; ?></blockquote>
+		<?php
+	} else {
+		?>
+		<q><?php echo $content; ?></q>
+		<?php
+	}
 }
 ?>
